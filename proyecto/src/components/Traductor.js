8c86cd161fe89c9dd220325acb056ed751e0ac04
milagros alonso <malonsoboto@udesa.edu.ar>
2025-11-10 20:42:50 -0300

    arreglo traductor

--- proyecto/src/components/Traductor.js
+++ proyecto/src/components/Traductor.js
@@ -4,10 +4,6 @@ function traductor(error) {
 
     if (error.code === "auth/invalid-email") {
         result = "Ingrese una direccion de correo valida";
-    } else if (error.code === "auth/wrong-password") {
-        result = "La contraseña es incorrecta";
-    } else if (error.code === "auth/user-not-found") {
-        result = "No hay una cuenta creada con esa direccion de correo";
     } else if (error.code === "auth/email-already-in-use") {
         result = "Ya existe una cuenta creada con esa direccion de correo";
     } else {
